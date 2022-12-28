9cf35433e61f1b5aabe936bc497a5554ecd55c7a
Maria <mengozzi.maria@gmail.com>
2022-12-28 16:26:37 +0100

    chore: change project structure in order to implement a microservice architecture

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,29 @@
 plugins {
     id("java")
-    alias(libs.plugins.gitSemVer)
+    id("org.danilopianini.git-sensitive-semantic-versioning-gradle-plugin") version "0.3.24"
+    //alias(libs.plugins.gitSemVer)
 }
 
 group = "it.unibo.smartgh"
 version = "1.0-SNAPSHOT"
 
+var vertx = "4.3.3"
+var junit = "5.8.1"
+
 repositories {
     mavenCentral()
 }
-
-dependencies {
-    implementation(libs.vertx.core)
-    implementation(libs.vertx.web)
-    implementation(libs.vertx.web.client)
-    implementation(libs.vertx.mqtt)
-    testImplementation(libs.junit.api)
-    testRuntimeOnly(libs.junit.engine)
+allprojects{
+    apply(plugin = "java")
+    apply(plugin = "org.danilopianini.git-sensitive-semantic-versioning-gradle-plugin")
+    dependencies {
+        implementation("io.vertx:vertx-core:$vertx")
+        implementation("io.vertx:vertx-web:$vertx")
+        implementation("io.vertx:vertx-web-client:$vertx")
+        implementation("io.vertx:vertx-mqtt:$vertx")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:$junit")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit")
+    }
 }
 
 tasks.getByName<Test>("test") {
--- src/main/java/it/unibo/smartgh/Main.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.smartgh;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
