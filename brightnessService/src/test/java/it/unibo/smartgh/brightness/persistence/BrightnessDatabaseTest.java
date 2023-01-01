f52297d5d6de1a72389160c8d294f315bce7500c
Elena Yan <elena.yan@studio.unibo.it>
2023-01-01 19:43:56 +0100

    test(brightness): fix brightness database test

--- brightnessService/src/test/java/it/unibo/smartgh/brightness/persistence/BrightnessDatabaseTest.java
+++ brightnessService/src/test/java/it/unibo/smartgh/brightness/persistence/BrightnessDatabaseTest.java
@@ -1,7 +1,10 @@
 package it.unibo.smartgh.brightness.persistence;
 
-import it.unibo.smartgh.brightness.entity.BrightnessValue;
-import org.junit.jupiter.api.BeforeAll;
+import it.unibo.smartgh.customException.EmptyDatabaseException;
+import it.unibo.smartgh.entity.PlantValue;
+import it.unibo.smartgh.entity.PlantValueImpl;
+import it.unibo.smartgh.plantValue.persistence.PlantValueDatabase;
+import it.unibo.smartgh.plantValue.persistence.PlantValueDatabaseImpl;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -11,32 +14,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BrightnessDatabaseTest {
 
+    private static final String BRIGHTNESS_DB_NAME = "brightness";
+    private static final String BRIGHTNESS_COLLECTION_NAME = "brightnessValues";
     private static final String HOST = "localhost";
     private static final int PORT = 27017;
-    private static final BrightnessDatabase brightnessDatabase = new BrightnessDatabaseImpl();
-
-    @BeforeAll
-    static void testConnection() {
-        assertDoesNotThrow(() -> brightnessDatabase.connection(HOST, PORT));
-    }
+    private static final PlantValueDatabase brightnessDatabase = new PlantValueDatabaseImpl(BRIGHTNESS_DB_NAME, BRIGHTNESS_COLLECTION_NAME, HOST, PORT);
 
     @Test
     public void testInsertAndGetCurrentBrightnessValue() {
         String greenHouseId = "1";
         Date date = new Date();
         Double valueRegistered = 1000.0;
-        BrightnessValue brightnessValue = new BrightnessValue(greenHouseId, date, valueRegistered);
-        brightnessDatabase.insertBrightnessValue(brightnessValue);
+        PlantValue brightnessValue = new PlantValueImpl(greenHouseId, date, valueRegistered);
+        brightnessDatabase.insertPlantValue(brightnessValue);
 
-        assertEquals(brightnessValue, brightnessDatabase.getBrightnessCurrentValue());
+        try {
+            assertEquals(brightnessValue, brightnessDatabase.getPlantCurrentValue());
+        } catch (EmptyDatabaseException e) {
+            e.printStackTrace();
+            fail();
+        }
     }
 
     @Test
     public void testGetHistoryData() {
         int howMany = 5;
-        List<BrightnessValue> list = brightnessDatabase.getHistoryData(howMany);
+        List<PlantValue> list = brightnessDatabase.getHistoryData(howMany);
         assertNotNull(list);
         assertTrue(list.size() <= howMany);
     }
-
 }
