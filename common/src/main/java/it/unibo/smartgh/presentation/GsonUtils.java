ea5bc0f96686066629d54338c7dd68cac08a6407
Elena Yan <elena.yan@studio.unibo.it>
2023-01-01 19:41:18 +0100

    feat(presentation): implement serializer and deserializer

---
+++ common/src/main/java/it/unibo/smartgh/presentation/GsonUtils.java
@@ -0,0 +1,24 @@
+package it.unibo.smartgh.presentation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import it.unibo.smartgh.entity.PlantValueImpl;
+import java.util.Date;
+import it.unibo.smartgh.presentation.deserializer.*;
+import it.unibo.smartgh.presentation.serializer.*;
+
+
+/**
+ * This is a utility class to instantiate the {@link com.google.gson.JsonSerializer} and {@link com.google.gson.JsonDeserializer}
+ */
+public class GsonUtils {
+
+    public static Gson createGson() {
+        return new GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .serializeNulls()
+            .registerTypeAdapter(PlantValueImpl.class, new PlantValueSerializer())
+            .registerTypeAdapter(PlantValueImpl.class, new PlantValueDeserializer())
+            .create();
+    }
+}
---
+++ common/src/main/java/it/unibo/smartgh/presentation/serializer/PlantValueSerializer.java
@@ -0,0 +1,25 @@
+package it.unibo.smartgh.presentation.serializer;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import it.unibo.smartgh.entity.PlantValueImpl;
+
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class PlantValueSerializer implements JsonSerializer<PlantValueImpl> {
+
+    @Override
+    public JsonElement serialize(PlantValueImpl src, Type typeOfSrc, JsonSerializationContext context) {
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
+        JsonObject object = new JsonObject();
+        object.addProperty("greenhouseId", src.getGreenhouseId());
+        object.addProperty("date", formatter.format(src.getDate()));
+        object.addProperty("value", src.getValue());
+
+        return object;
+    }
+}
