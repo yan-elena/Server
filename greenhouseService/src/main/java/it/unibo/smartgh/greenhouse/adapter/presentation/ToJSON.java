ad5ca3f2af01638b22ceaf4ae48a414393d5d3bd
Maria <mengozzi.maria@gmail.com>
2023-01-02 10:42:15 +0100

    feat(greenhouseService): add route for getting modality and parameters

--- greenhouseService/src/main/java/it/unibo/smartgh/greenhouse/adapter/presentation/ToJSON.java
+++ greenhouseService/src/main/java/it/unibo/smartgh/greenhouse/adapter/presentation/ToJSON.java
@@ -5,6 +5,10 @@ import it.unibo.smartgh.greenhouse.entity.Greenhouse;
 import it.unibo.smartgh.greenhouse.entity.Modality;
 import it.unibo.smartgh.greenhouse.entity.Plant;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class ToJSON {
     public static JsonObject greenhouseToJSON(Greenhouse gh){
         JsonObject greenhouse = new JsonObject();
@@ -27,5 +31,33 @@ public class ToJSON {
         plant.put("minHumidity", p.getMinHumidity());
         plant.put("maxHumidity", p.getMaxHumidity());
         return plant;
+    }
+
+    public static JsonObject modalityToJSON(Modality mod){
+        JsonObject modality = new JsonObject();
+        modality.put("modality", mod.name());
+        return modality;
+    }
+
+    public static JsonObject paramToJSON(Plant plant, String param){
+        JsonObject parameter = new JsonObject();
+        String original = param.toLowerCase();
+        String paramName = original.substring(0, 1).toUpperCase() + original.substring(1);
+        try {
+            Class<?> c = Class.forName(Plant.class.getName());
+            Method minMethod = c.getDeclaredMethod("getMin"+paramName);
+            Method maxMethod = c.getDeclaredMethod("getMax"+paramName);
+            parameter.put("min"+paramName, minMethod.invoke(plant));
+            parameter.put("max"+paramName, maxMethod.invoke(plant));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        return parameter;
     }
 }
