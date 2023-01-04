d96df1de59907ace178ed69d49c7942c9fc398bc
Elena Yan <elena.yan@studio.unibo.it>
2023-01-04 17:29:32 +0100

    feat(temperature): implement temperature launcher and http adapter

---
+++ temperatureService/src/main/java/temperature/adapter/TemperatureHTTPAdapter.java
@@ -0,0 +1,117 @@
+package temperature.adapter;
+
+import com.google.gson.JsonParseException;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import it.unibo.smartgh.adapter.AbstractAdapter;
+import it.unibo.smartgh.customException.EmptyDatabaseException;
+import it.unibo.smartgh.entity.PlantValue;
+import it.unibo.smartgh.entity.PlantValueImpl;
+import it.unibo.smartgh.plantValue.api.PlantValueAPI;
+
+import java.util.List;
+
+public class TemperatureHTTPAdapter extends AbstractAdapter<PlantValueAPI> {
+
+    private static final String BASE_PATH = "/temperature";
+    private static final String HISTORY_PATH = BASE_PATH + "/history/:howMany";
+    private static final String BAD_REQUEST_MESSAGE = "Bad request: some field is missing or invalid in the provided data.";
+
+    private final String host;
+    private final int port;
+
+    public TemperatureHTTPAdapter(PlantValueAPI model, Vertx vertx, String host, int port) {
+        super(model, vertx);
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public void setupAdapter(Promise<Void> startPromise) {
+        HttpServer server = getVertx().createHttpServer();
+        Router router = Router.router(this.getVertx());
+        try{
+            router.route().handler(BodyHandler.create());
+
+            router.get(BASE_PATH).handler(this::handleGetTemperatureCurrentValue);
+            router.get(HISTORY_PATH).handler(this::handleGetTemperatureHistoryData);
+
+            router.post(BASE_PATH).handler(this::handlePostTemperatureValue);
+
+            server.requestHandler(router).listen(port, host, http -> {
+                if (http.succeeded()) {
+                    startPromise.complete();
+                    System.out.println("HTTP Thing Adapter started on port " + port);
+                } else {
+                    System.out.println("HTTP Thing Adapter failure " + http.cause());
+                    startPromise.fail(http.cause());
+                }
+            });
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("API setup failed - " + ex);
+            startPromise.fail("API setup failed - " + ex);
+        }
+    }
+
+    private void handleGetTemperatureCurrentValue(RoutingContext ctx) {
+        HttpServerResponse res = ctx.response();
+        res.putHeader("Content-Type", "application/json");
+        Future<PlantValue> fut = this.getModel().getCurrentValue();
+
+        fut.onSuccess(brightnessValue -> res.end(gson.toJson(brightnessValue, PlantValueImpl.class)))
+                .onFailure(exception -> handleFailureInGetMethod(res, exception));
+    }
+
+    private void handleGetTemperatureHistoryData(RoutingContext ctx) {
+        int howMany = ctx.get("howMany");
+        HttpServerResponse res = ctx.response();
+        res.putHeader("Content-Type", "application/json");
+        Future<List<PlantValue>> fut = this.getModel().getHistory(howMany);
+
+        fut.onSuccess(list -> res.end(gson.toJson(list)))
+                .onFailure(exception -> handleFailureInGetMethod(res, exception));
+    }
+
+    private void handlePostTemperatureValue(RoutingContext request) {
+        HttpServerResponse response = request.response();
+        response.putHeader("content-type", "application/json");
+
+        try {
+            PlantValue brightnessValue = gson.fromJson(request.body().asString(), PlantValueImpl.class);
+            Future<Void> fut = this.getModel().postValue(brightnessValue);
+
+            fut.onSuccess(res -> {
+                response.setStatusCode(201);
+                response.end();
+            }).onFailure(exception -> {
+                response.setStatusCode(500);
+                response.setStatusMessage("Internal Server error: cause" + exception.getMessage());
+                response.end();
+            });
+        } catch (JsonParseException e) {
+            response.setStatusCode(400);
+            response.setStatusMessage(BAD_REQUEST_MESSAGE);
+            response.end();
+        }
+    }
+
+    private void handleFailureInGetMethod(HttpServerResponse res, Throwable exception) {
+        if (exception instanceof EmptyDatabaseException) {
+            res.setStatusCode(200);
+            res.end();
+        } else {
+            res.setStatusCode(500);
+            res.setStatusMessage("Internal Server error: cause" + exception.getMessage());
+            res.end();
+        }
+    }
+
+}
---
+++ temperatureService/src/main/java/temperature/service/TemperatureService.java
@@ -0,0 +1,55 @@
+package temperature.service;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import it.unibo.smartgh.adapter.AbstractAdapter;
+import it.unibo.smartgh.plantValue.api.PlantValueAPI;
+import it.unibo.smartgh.plantValue.api.PlantValueModel;
+import temperature.adapter.TemperatureHTTPAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TemperatureService extends AbstractVerticle {
+
+    private final List<AbstractAdapter<PlantValueAPI>> adapters;
+    private final PlantValueAPI model;
+    private final String host;
+    private final int port;
+
+    public TemperatureService(PlantValueModel model, String host, int port) {
+        this.adapters = new LinkedList<>();
+        this.model = model;
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) {
+        System.out.println("TemperatureService started.");
+        installAdapters(startPromise);
+    }
+
+    private void installAdapters(Promise<Void> startPromise) {
+        try {
+            TemperatureHTTPAdapter httpAdapter = new TemperatureHTTPAdapter(model, this.getVertx(), host, port);
+            Promise<Void> promise = Promise.promise();
+            httpAdapter.setupAdapter(promise);
+            Future<Void> fut = promise.future();
+            fut.onSuccess(res -> {
+                System.out.println("HTTP adapter installed.");
+                adapters.add(httpAdapter);
+                startPromise.complete();
+            }).onFailure(f -> {
+                startPromise.fail("HTTP adapter not installed");
+                System.out.println("HTTP adapter not installed");
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            startPromise.fail("HTTP adapter installation failed.");
+            System.out.println("HTTP adapter installation failed.");
+
+        }
+    }
+}
