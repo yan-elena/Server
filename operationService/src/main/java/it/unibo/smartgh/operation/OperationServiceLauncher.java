0c203d679bb883dc02938f2a0174027a5e3b54ef
Elena Yan <elena.yan@studio.unibo.it>
2023-01-04 23:26:10 +0100

    feat(operation): implement operation service and adapter

---
+++ operationService/src/main/java/it/unibo/smartgh/operation/OperationServiceLauncher.java
@@ -0,0 +1,28 @@
+package it.unibo.smartgh.operation;
+
+import io.vertx.core.Vertx;
+import it.unibo.smartgh.operation.api.OperationAPI;
+import it.unibo.smartgh.operation.api.OperationModel;
+import it.unibo.smartgh.operation.controller.OperationController;
+import it.unibo.smartgh.operation.controller.OperationControllerImpl;
+import it.unibo.smartgh.operation.persistence.OperationDatabase;
+import it.unibo.smartgh.operation.persistence.OperationDatabaseImpl;
+import it.unibo.smartgh.operation.service.OperationService;
+
+public class OperationServiceLauncher {
+
+    private static final String OPERATION_DB_NAME = "operation";
+    private static final String OPERATION_COLLECTION_NAME = "operation";
+    private static final int PORT = 8892;
+    private static final int MONGODB_PORT = 27017;
+    private static final String HOST = "localhost";
+    private static final String MONGODB_HOST = "localhost";
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        OperationDatabase database = new OperationDatabaseImpl(OPERATION_DB_NAME, OPERATION_COLLECTION_NAME, MONGODB_HOST, MONGODB_PORT);
+        OperationController controller = new OperationControllerImpl(database);
+        OperationAPI model = new OperationModel(controller);
+        vertx.deployVerticle(new OperationService(model, HOST, PORT));
+    }
+}
---
+++ operationService/src/main/java/it/unibo/smartgh/operation/service/OperationService.java
@@ -0,0 +1,54 @@
+package it.unibo.smartgh.operation.service;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Verticle;
+import it.unibo.smartgh.operation.adapter.AbstractAdapter;
+import it.unibo.smartgh.operation.adapter.OperationHTTPAdapter;
+import it.unibo.smartgh.operation.api.OperationAPI;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class OperationService extends AbstractVerticle {
+
+    private final List<AbstractAdapter<OperationAPI>> adapters;
+    private final OperationAPI model;
+    private final String host;
+    private final int port;
+
+    public OperationService(OperationAPI model, String host, int port) {
+        this.adapters = new LinkedList<>();
+        this.model = model;
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public void start(Promise<Void> startPromise) {
+        System.out.println("BrightnessService started.");
+        installAdapters(startPromise);
+    }
+
+    private void installAdapters(Promise<Void> startPromise) {
+        try {
+            OperationHTTPAdapter httpAdapter = new OperationHTTPAdapter(model, this.getVertx(), host, port);
+            Promise<Void> promise = Promise.promise();
+            httpAdapter.setupAdapter(promise);
+            Future<Void> fut = promise.future();
+            fut.onSuccess(res -> {
+                System.out.println("HTTP adapter installed.");
+                adapters.add(httpAdapter);
+                startPromise.complete();
+            }).onFailure(f -> {
+                startPromise.fail("HTTP adapter not installed");
+                System.out.println("HTTP adapter not installed");
+            });
+        }  catch (Exception ex) {
+            ex.printStackTrace();
+            startPromise.fail("HTTP adapter installation failed.");
+            System.out.println("HTTP adapter installation failed.");
+        }
+    }
+}
