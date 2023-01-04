cb2e13ab9dc94fcedf718f26c414441090851b55
Maria <mengozzi.maria@gmail.com>
2023-01-04 16:51:50 +0100

    feat(humidityService): add submodule that represents the humidity service

---
+++ humidityService/src/main/java/it/unibo/smartgh/humidity/adapter/HumidityHTTPAdapter.java
@@ -0,0 +1,115 @@
+package it.unibo.smartgh.humidity.adapter;
+
+import com.google.gson.JsonParseException;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
+import it.unibo.smartgh.adapter.AbstractAdapter;
+import it.unibo.smartgh.entity.PlantValue;
+import it.unibo.smartgh.entity.PlantValueImpl;
+import it.unibo.smartgh.plantValue.api.PlantValueAPI;
+import it.unibo.smartgh.customException.EmptyDatabaseException;
+
+import java.util.List;
+
+public class HumidityHTTPAdapter extends AbstractAdapter<PlantValueAPI> {
+
+    private static final String BASE_PATH = "/humidity";
+    private static final String HISTORY_PATH = BASE_PATH + "/history/:howMany";
+    private static final String BAD_REQUEST_MESSAGE = "Bad request: some field is missing or invalid in the provided data.";
+
+    private final String host;
+    private final int port;
+
+    public HumidityHTTPAdapter(PlantValueAPI model, Vertx vertx, String host, int port) {
+        super(model, vertx);
+        this.host = host;
+        this.port = port;
+    }
+
+    @Override
+    public void setupAdapter(Promise<Void> startPromise) {
+        HttpServer server = getVertx().createHttpServer();
+        Router router = Router.router(this.getVertx());
+        try{
+            router.route().handler(BodyHandler.create());
+
+            router.get(BASE_PATH).handler(this::handleGetHumidityCurrentValue);
+            router.get(HISTORY_PATH).handler(this::handleGetHumidityHistoryData);
+
+            router.post(BASE_PATH).handler(this::handlePostHumidityValue);
+
+            server.requestHandler(router).listen(port, host, http -> {
+                if (http.succeeded()) {
+                    startPromise.complete();
+                    System.out.println("HTTP Thing Adapter started on port " + port);
+                } else {
+                    System.out.println("HTTP Thing Adapter failure " + http.cause());
+                    startPromise.fail(http.cause());
+                }
+            });
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("API setup failed - " + ex);
+            startPromise.fail("API setup failed - " + ex);
+        }
+    }
+
+    private void handleGetHumidityCurrentValue(RoutingContext ctx) {
+        HttpServerResponse res = ctx.response();
+        res.putHeader("Content-Type", "application/json");
+        Future<PlantValue> fut = this.getModel().getCurrentValue();
+
+        fut.onSuccess(humidityValue -> res.end(gson.toJson(humidityValue, PlantValueImpl.class)))
+                .onFailure(exception -> handleFailureInGetMethod(res, exception));
+    }
+
+    private void handleGetHumidityHistoryData(RoutingContext ctx){
+        Integer howMany = ctx.get("howMany");
+        HttpServerResponse res = ctx.response();
+        res.putHeader("Content-Type", "application/json");
+        Future<List<PlantValue>> fut = this.getModel().getHistory(howMany);
+        fut.onSuccess(list -> res.end(gson.toJson(list)))
+                .onFailure(exception -> handleFailureInGetMethod(res, exception));
+    }
+
+    private void handlePostHumidityValue(RoutingContext request) {
+        HttpServerResponse response = request.response();
+        response.putHeader("content-type", "application/json");
+
+        try {
+            PlantValue humidityValue = gson.fromJson(request.body().asString(), PlantValueImpl.class);
+            Future<Void> fut = this.getModel().postValue(humidityValue);
+
+            fut.onSuccess(res -> {
+                response.setStatusCode(201);
+                response.end();
+            }).onFailure(exception -> {
+                response.setStatusCode(500);
+                response.setStatusMessage("Internal Server error: cause" + exception.getMessage());
+                response.end();
+            });
+        } catch (JsonParseException e) {
+             response.setStatusCode(400);
+             response.setStatusMessage(BAD_REQUEST_MESSAGE);
+             response.end();
+        }
+    }
+
+    private void handleFailureInGetMethod(HttpServerResponse res, Throwable exception) {
+        if (exception instanceof EmptyDatabaseException) {
+            res.setStatusCode(200);
+            res.end();
+        } else {
+            res.setStatusCode(500);
+            res.setStatusMessage("Internal Server error: cause" + exception.getMessage());
+            res.end();
+        }
+    }
+}
---
+++ humidityService/src/test/java/it/unibo/smartgh/humidity/persistence/HumidityDatabaseTest.java
@@ -0,0 +1,46 @@
+package it.unibo.smartgh.humidity.persistence;
+
+import it.unibo.smartgh.customException.EmptyDatabaseException;
+import it.unibo.smartgh.entity.PlantValue;
+import it.unibo.smartgh.entity.PlantValueImpl;
+import it.unibo.smartgh.plantValue.persistence.PlantValueDatabase;
+import it.unibo.smartgh.plantValue.persistence.PlantValueDatabaseImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HumidityDatabaseTest {
+
+    private static final String HUMIDITY_DB_NAME = "humidity";
+    private static final String HUMIDITY_COLLECTION_NAME = "humidityValues";
+    private static final String HOST = "localhost";
+    private static final int PORT = 27017;
+    private static final PlantValueDatabase humidityDatabase = new PlantValueDatabaseImpl(HUMIDITY_DB_NAME, HUMIDITY_COLLECTION_NAME, HOST, PORT);
+
+    @Test
+    public void testInsertAndGetCurrentHumidityValue() {
+        String greenHouseId = "1";
+        Date date = new Date();
+        Double valueRegistered = 1000.0;
+        PlantValue humidityValue = new PlantValueImpl(greenHouseId, date, valueRegistered);
+        humidityDatabase.insertPlantValue(humidityValue);
+
+        try {
+            assertEquals(humidityValue, humidityDatabase.getPlantCurrentValue());
+        } catch (EmptyDatabaseException e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void testGetHistoryData() {
+        int howMany = 5;
+        List<PlantValue> list = humidityDatabase.getHistoryData(howMany);
+        assertNotNull(list);
+        assertTrue(list.size() <= howMany);
+    }
+}
