4ed09ed237602c5b264d7486106f755e8a9d7464
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-01-04 19:43:44 +0100

    feat(greenhouseCommunication): add class and interface to handle the http part of the service

---
+++ greenhouseCommunicationGateway/src/main/java/it/unibo/smartgh/greenhouseCommunication/api/http/GreenhouseCommunicationHTTPModel.java
@@ -0,0 +1,47 @@
+package it.unibo.smartgh.greenhouseCommunication.api.http;
+
+
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class GreenhouseCommunicationHTTPModel implements GreenhouseCommunicationHTTPAPI {
+
+    private static final String BRIGHTNESS_OPERATION_TOPIC = "LUMINOSITY";
+    private static final String SOIL_MOISTURE_OPERATION_TOPIC = "IRRIGATION";
+    private static final String TEMPERATURE_OPERATION_TOPIC = "TEMPERATURE";
+    private static final String AIR_HUMIDITY_OPERATION_TOPIC = "VENTILATION";
+
+    private Vertx vertx;
+
+    public GreenhouseCommunicationHTTPModel(Vertx vertx) {
+        this.vertx = vertx;
+    }
+
+    @Override
+    public Future<Void> postBrightnessOperation(String command) {
+        return this.sendOperationMessage(BRIGHTNESS_OPERATION_TOPIC, command);
+    }
+
+    @Override
+    public Future<Void> postSoilMoistureOperation(String command) {
+        return this.sendOperationMessage(SOIL_MOISTURE_OPERATION_TOPIC, command);
+    }
+
+    @Override
+    public Future<Void> postAirHumidityOperator(String command) {
+        return this.sendOperationMessage(AIR_HUMIDITY_OPERATION_TOPIC, command);
+    }
+
+    @Override
+    public Future<Void> postTemperatureOperation(String command) {
+        return this.sendOperationMessage(TEMPERATURE_OPERATION_TOPIC, command);
+    }
+
+    private Future<Void> sendOperationMessage(String topic, String command) {
+        Promise<Void> promise = Promise.promise();
+        this.vertx.eventBus().publish(topic, command);
+        promise.complete();
+        return promise.future();
+    }
+}
