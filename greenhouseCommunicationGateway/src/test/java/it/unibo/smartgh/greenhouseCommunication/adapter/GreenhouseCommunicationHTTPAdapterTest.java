b4d9f72c963f3d29647561541f40d0833e0880c2
AnnaVitali <anna.vitali4@studio.unibo.it>
2023-01-06 16:16:30 +0100

    test(greenhouseCommunicationService): create test for greenhouse communication service

---
+++ greenhouseCommunicationGateway/src/test/java/it/unibo/smartgh/greenhouseCommunication/adapter/GreenhouseCommunicationHTTPAdapterTest.java
@@ -0,0 +1,103 @@
+package it.unibo.smartgh.greenhouseCommunication.adapter;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import it.unibo.smartgh.greenhouseCommunication.api.http.GreenhouseCommunicationHTTPModel;
+import it.unibo.smartgh.greenhouseCommunication.api.mqtt.GreenhouseCommunicationMQTTModel;
+import it.unibo.smartgh.greenhouseCommunication.service.GreenhouseCommunicationService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(VertxExtension.class)
+public class GreenhouseCommunicationHTTPAdapterTest {
+
+    private static final String HTTP_HOST = "localhost";
+    private static final int HTTP_PORT = 8892;
+    private static final String MQTT_HOST = "broker.mqtt-dashboard.com";
+    private static final int MQTT_PORT = 1883;
+
+    private static final String GREENHOUSE_ID = "63af0ae025d55e9840cbc1fa";
+
+    @BeforeEach
+    public void startService(Vertx vertx, VertxTestContext testContext){
+        System.out.println("Greenhouse Communication service initializing");
+        GreenhouseCommunicationHTTPModel httpModel = new GreenhouseCommunicationHTTPModel(vertx);
+        GreenhouseCommunicationMQTTModel mqttModel = new GreenhouseCommunicationMQTTModel(GREENHOUSE_ID, vertx);
+        GreenhouseCommunicationService service = new GreenhouseCommunicationService(httpModel, mqttModel, MQTT_HOST, HTTP_HOST, MQTT_PORT, HTTP_PORT);
+        vertx.deployVerticle(service, testContext.succeedingThenComplete());
+        System.out.println("Greenhouse Communication service ready");
+    }
+    @Test
+    public void postBrightnessOperationTest(Vertx vertx, VertxTestContext testContext) {
+        WebClient client = WebClient.create(vertx);
+        JsonObject body = new JsonObject();
+        String operationPath = "/greenhouseCommunication/brightnessOperation";
+        int expectedStatusCode = 201;
+
+        body.put("message", "LUMINOSITY 200");
+        client.post(HTTP_PORT, HTTP_HOST, operationPath)
+                .putHeader("content-type", "application/json")
+                .sendJsonObject(body)
+                .onSuccess(response -> testContext.verify(() -> {
+                    assertEquals(response.statusCode(), expectedStatusCode);
+                    testContext.completeNow();
+                }));
+    }
+
+    @Test
+    public void postAirHumidityOperationTest(Vertx vertx, VertxTestContext testContext) {
+        WebClient client = WebClient.create(vertx);
+        JsonObject body = new JsonObject();
+        String operationPath = "/greenhouseCommunication/airHumidityOperation";
+        int expectedStatusCode = 201;
+
+        body.put("message", "VENTILATION on");
+        client.post(HTTP_PORT, HTTP_HOST, operationPath)
+                .putHeader("content-type", "application/json")
+                .sendJsonObject(body)
+                .onSuccess(response -> testContext.verify(() -> {
+                    assertEquals(response.statusCode(), expectedStatusCode);
+                    testContext.completeNow();
+                }));
+    }
+
+    @Test
+    public void postSoilMoistureOperationTest(Vertx vertx, VertxTestContext testContext) {
+        WebClient client = WebClient.create(vertx);
+        JsonObject body = new JsonObject();
+        String operationPath = "/greenhouseCommunication/soilMoistureOperation";
+        int expectedStatusCode = 201;
+
+        body.put("message", "IRRIGATION on");
+        client.post(HTTP_PORT, HTTP_HOST, operationPath)
+                .putHeader("content-type", "application/json")
+                .sendJsonObject(body)
+                .onSuccess(response -> testContext.verify(() -> {
+                    assertEquals(response.statusCode(), expectedStatusCode);
+                    testContext.completeNow();
+                }));
+    }
+
+    @Test
+    public void postTemperatureOperationTest(Vertx vertx, VertxTestContext testContext) {
+        WebClient client = WebClient.create(vertx);
+        JsonObject body = new JsonObject();
+        String operationPath = "/greenhouseCommunication/temperatureOperation";
+        int expectedStatusCode = 201;
+
+        body.put("message", "TEMPERATURE decrease");
+        client.post(HTTP_PORT, HTTP_HOST, operationPath)
+                .putHeader("content-type", "application/json")
+                .sendJsonObject(body)
+                .onSuccess(response -> testContext.verify(() -> {
+                    assertEquals(response.statusCode(), expectedStatusCode);
+                    testContext.completeNow();
+                }));
+    }
+}
