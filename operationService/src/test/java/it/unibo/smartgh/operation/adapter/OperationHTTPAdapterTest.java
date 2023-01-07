b971603f22c633ab74aea416632504bf42866ad3
Elena Yan <elena.yan@studio.unibo.it>
2023-01-07 19:23:16 +0100

    test(operations): implement operation http adapter test

---
+++ operationService/src/test/java/it/unibo/smartgh/operation/adapter/OperationHTTPAdapterTest.java
@@ -0,0 +1,135 @@
+package it.unibo.smartgh.operation.adapter;
+
+import com.google.gson.Gson;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import it.unibo.smartgh.operation.api.OperationAPI;
+import it.unibo.smartgh.operation.api.OperationModel;
+import it.unibo.smartgh.operation.controller.OperationController;
+import it.unibo.smartgh.operation.controller.OperationControllerImpl;
+import it.unibo.smartgh.operation.entity.Modality;
+import it.unibo.smartgh.operation.entity.Operation;
+import it.unibo.smartgh.operation.entity.OperationImpl;
+import it.unibo.smartgh.operation.persistence.OperationDatabase;
+import it.unibo.smartgh.operation.persistence.OperationDatabaseImpl;
+import it.unibo.smartgh.operation.presentation.GsonUtils;
+import it.unibo.smartgh.operation.service.OperationService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(VertxExtension.class)
+public class OperationHTTPAdapterTest {
+
+    private static final String OPERATION_DB_NAME = "operation";
+    private static final String OPERATION_COLLECTION_NAME = "operation";
+    private static final String DB_HOST = "localhost";
+    private static final int DB_PORT = 27017;
+    private static final String SERVICE_HOST = "localhost";
+    private static final int SERVICE_PORT = 8896;
+
+    private static final OperationDatabase database = new OperationDatabaseImpl(OPERATION_DB_NAME, OPERATION_COLLECTION_NAME,
+            DB_HOST, DB_PORT);
+    private final Gson gson = GsonUtils.createGson();
+    private static final String greenhouseId = "1";
+    private static final String parameter = "temperature";
+    private final int limit = 5;
+
+    @BeforeAll
+    static public void insertOperationsInDB(Vertx vertx, VertxTestContext testContext) {
+        System.out.println("Operation service initializing");
+        final String action = "TEMPERATURE decrease";
+        final DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            final Date date1 = formatter.parse("01/11/2021");
+            final Date date2 = formatter.parse("21/08/2022");
+            final Date date3 = formatter.parse("15/02/2023");
+            database.insertOperation(new OperationImpl(greenhouseId, Modality.AUTOMATIC, date1, parameter, action));
+            database.insertOperation(new OperationImpl(greenhouseId, Modality.AUTOMATIC, date2, parameter, action));
+            database.insertOperation(new OperationImpl(greenhouseId, Modality.MANUAL, date3, parameter, action));
+        } catch (ParseException e) {
+            testContext.failNow(e);
+        }
+
+        OperationController controller = new OperationControllerImpl(database);
+        OperationAPI model = new OperationModel(controller, vertx);
+        OperationService service = new OperationService(model, SERVICE_HOST, SERVICE_PORT);
+        vertx.deployVerticle(service, testContext.succeedingThenComplete());
+        try {
+            testContext.awaitCompletion(10, TimeUnit.SECONDS);
+            System.out.println("wait");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetOperations(Vertx vertx, VertxTestContext testContext) {
+        WebClient client = WebClient.create(vertx);
+        client.get(SERVICE_PORT, SERVICE_HOST, "/operations/" + greenhouseId + "/" + limit)
+                .send(testContext.succeeding(res -> testContext.verify(() -> {
+                    JsonArray array = res.body().toJsonArray();
+                    assertTrue(array.size() <= limit);
+                    array.forEach(o -> {
+                        Operation op = gson.fromJson(o.toString(), OperationImpl.class);
+                        assertEquals(greenhouseId, op.getGreenhouseId());
+                    });
+                    testContext.completeNow();
+                })));
+    }
+
+    @Test
+    void testGetParameterOperations(Vertx vertx, VertxTestContext testContext) {
+        WebClient client = WebClient.create(vertx);
+        client.get(SERVICE_PORT, SERVICE_HOST, "/operations/" + greenhouseId + "/" + parameter + "/" + limit)
+                .send(testContext.succeeding(res -> testContext.verify(() -> {
+                    JsonArray array = res.body().toJsonArray();
+                    assertTrue(array.size() <= limit);
+                    array.forEach(o -> {
+                        Operation op = gson.fromJson(o.toString(), OperationImpl.class);
+                        assertEquals(greenhouseId, op.getGreenhouseId());
+                        assertEquals(parameter, op.getParameter());
+                    });
+                    testContext.completeNow();
+                })));
+    }
+
+    @Test
+    void testGetOperationsInDateRange(Vertx vertx, VertxTestContext testContext) {
+        final DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String fromDate = "2022-01-01";
+        String toDate = "2023-01-01";
+        try {
+            final Date from = formatter.parse(fromDate);
+            final Date to = formatter.parse(toDate);
+
+            WebClient client = WebClient.create(vertx);
+            client.get(SERVICE_PORT, SERVICE_HOST, "/operations/" + greenhouseId + "/" + fromDate + "/" + toDate + "/" + limit)
+                    .send(testContext.succeeding(res -> testContext.verify(() -> {
+                        JsonArray array = res.body().toJsonArray();
+                        assertTrue(array.size() <= limit);
+                        array.forEach(o -> {
+                            Operation op = gson.fromJson(o.toString(), OperationImpl.class);
+                            assertEquals(greenhouseId, op.getGreenhouseId());
+                            assertTrue(op.getDate().compareTo(from) > 0);
+                            assertTrue(op.getDate().compareTo(to) < 0);
+                        });
+                        testContext.completeNow();
+                    })));
+        } catch (ParseException e) {
+            testContext.failNow(e);
+        }
+    }
+}
