fb276ac8361c3278e920c6545cea707bdc5f1b59
Maria <mengozzi.maria@gmail.com>
2023-01-13 20:33:53 +0100

    refactor: add config file and dockerfile

--- humidityService/src/test/java/it/unibo/smartgh/humidity/persistence/HumidityDatabaseTest.java
+++ humidityService/src/test/java/it/unibo/smartgh/humidity/persistence/HumidityDatabaseTest.java
@@ -5,10 +5,14 @@ import it.unibo.smartgh.plantValue.entity.PlantValue;
 import it.unibo.smartgh.plantValue.entity.PlantValueImpl;
 import it.unibo.smartgh.plantValue.persistence.PlantValueDatabase;
 import it.unibo.smartgh.plantValue.persistence.PlantValueDatabaseImpl;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,10 +23,24 @@ class HumidityDatabaseTest {
 
     private static final String HUMIDITY_DB_NAME = "humidity";
     private static final String HUMIDITY_COLLECTION_NAME = "humidityValues";
-    private static final String HOST = "localhost";
-    private static final int PORT = 27017;
-    private static final PlantValueDatabase humidityDatabase = new PlantValueDatabaseImpl(HUMIDITY_DB_NAME, HUMIDITY_COLLECTION_NAME, HOST, PORT);
+    private static PlantValueDatabase humidityDatabase;
     private final String greenhouseId = "1";
+
+    @BeforeAll
+    public static void start() {
+        try {
+            InputStream is = HumidityDatabaseTest.class.getResourceAsStream("/config.properties");
+            Properties properties = new Properties();
+            properties.load(is);
+
+            String host = properties.getProperty("mongodb.host");
+            int port = Integer.parseInt(properties.getProperty("mongodb.port"));
+            System.out.println(host);
+            humidityDatabase = new PlantValueDatabaseImpl(HUMIDITY_DB_NAME, HUMIDITY_COLLECTION_NAME, host, port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     public void testInsertAndGetCurrentHumidityValue() {
